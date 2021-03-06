6e7113908f4eb54370d3a47e671b7dae2c459768
Sergey Smolnikov <ssmol@equinor.com>
2021-03-06 12:49:26 +0100

    Fixed set env-vars

--- api/handlers/job/handler.go
+++ api/handlers/job/handler.go
@@ -8,7 +8,6 @@ import (
 	"github.com/equinor/radix-operator/pkg/apis/kube"
 	radixv1 "github.com/equinor/radix-operator/pkg/apis/radix/v1"
 	"github.com/equinor/radix-operator/pkg/apis/utils"
-	"github.com/equinor/radix-operator/pkg/apis/utils/git"
 	radixclient "github.com/equinor/radix-operator/pkg/client/clientset/versioned"
 	log "github.com/sirupsen/logrus"
 	batchv1 "k8s.io/api/batch/v1"
@@ -17,6 +16,12 @@ import (
 	"k8s.io/client-go/kubernetes"
 	"strings"
 	"time"
+)
+
+const (
+	JOB_PAYLOAD_PROPERTY_NAME          = "payload"
+	JOB_SERVICE_ACCOUNT_NAME           = "radix-job-scheduler"
+	OBSOLETE_RADIX_APP_NAME_LABEL_NAME = "radix-app-name"
 )
 
 type Handler interface {
@@ -78,20 +83,41 @@ func (jh *jobHandler) CreateJob(jobScheduleDescription *models.JobScheduleDescri
 	radixV1 := *jh.radixClient
 	namespace := jh.env.RadixDeploymentNamespace
 	log.Debugf("create job for namespace: %s", namespace)
+
 	radixDeployment, err := radixV1.RadixV1().RadixDeployments(namespace).Get(jobScheduleDescription.RadixDeploymentName, metav1.GetOptions{})
 	if err != nil {
 		return nil, errors.New(fmt.Sprintf("fail to get Radix deployment %s for namespace: %s", jobScheduleDescription.RadixDeploymentName, namespace))
 	}
-	newJob, _ := createJob(jh.kube, radixDeployment, jobScheduleDescription)
-	job, err := kubeClient.BatchV1().Jobs(namespace).Create(newJob)
+
+	job, _ := buildJob(jh.kube, radixDeployment, jobScheduleDescription)
+
+	secret := createPayloadSecret(job, jobScheduleDescription)
+	_, err = kubeClient.CoreV1().Secrets(namespace).Create(secret)
 	if err != nil {
 		return nil, err
 	}
-	if job == nil {
-		return nil, errors.New(fmt.Sprintf("not created job for component %s, environment %s, in namespace: %s", jobScheduleDescription.ComponentName, jobScheduleDescription.Environment, namespace))
+
+	createdJob, err := kubeClient.BatchV1().Jobs(namespace).Create(job)
+	if err != nil {
+		return nil, err
 	}
-	log.Debugf("found Job %s for namespace: %s", newJob.Name, namespace)
-	return nil, nil
+
+	log.Debug(fmt.Sprintf("created job for component %s, environment %s, in namespace: %s", jobScheduleDescription.ComponentName, radixDeployment.Spec.Environment, namespace))
+	return models.GetJobStatusFromJob(createdJob), nil
+}
+
+func createPayloadSecret(job *batchv1.Job, jobScheduleDescription *models.JobScheduleDescription) *corev1.Secret {
+	secret := corev1.Secret{
+		Type: corev1.SecretTypeOpaque,
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      getPayloadSecretName(job.Name),
+			Namespace: job.Namespace,
+		},
+		Data: map[string][]byte{
+			JOB_PAYLOAD_PROPERTY_NAME: []byte(jobScheduleDescription.Payload),
+		},
+	}
+	return &secret
 }
 
 func (jh *jobHandler) DeleteJob(jobName string) error {
@@ -106,69 +132,93 @@ func (jh *jobHandler) DeleteJob(jobName string) error {
 	return nil
 }
 
-func createJob(kubeutil *kube.Kube, rd *radixv1.RadixDeployment, jobDesc *models.JobScheduleDescription) (*batchv1.Job, error) {
-	appName := rd.Spec.AppName
-	timestamp := time.Now().Format("20060102150405")
-	jobTag := strings.ToLower(utils.RandString(8))
-	componentName := jobDesc.ComponentName
-	//environment := jobDesc.Environment
-	radixComponent := getRadixJobComponentBy(rd, componentName)
-	if radixComponent == nil {
-		return nil, errors.New(fmt.Sprintf("radix job component %s not found in Radix Deployment %s", componentName, rd.Name))
+func buildJob(kubeutil *kube.Kube, rd *radixv1.RadixDeployment, jobDesc *models.JobScheduleDescription) (*batchv1.Job, error) {
+	jobComponentName := jobDesc.ComponentName
+	radixJobComponent := getRadixJobComponentBy(rd, jobComponentName)
+	if radixJobComponent == nil {
+		return nil, errors.New(fmt.Sprintf("radix Job Component %s not found in Radix Deployment %s", jobComponentName, rd.Name))
 	}
-	//imageTag := radixComponent.Image
-	jobName := fmt.Sprintf("%s-%s-%s", componentName, timestamp, jobTag)
 
-	job := batchv1.Job{
+	appName := rd.Spec.AppName
+	namespace := rd.ObjectMeta.Namespace
+
+	job := createJob(appName, namespace, jobComponentName)
+
+	containers := getContainers(kubeutil, rd, radixJobComponent)
+	containers[0].VolumeMounts = append(containers[0].VolumeMounts, corev1.VolumeMount{
+		Name:      JOB_PAYLOAD_PROPERTY_NAME,
+		ReadOnly:  true,
+		MountPath: radixJobComponent.Payload.Path,
+	})
+	volumes := getVolumes(namespace, rd.Spec.Environment, radixJobComponent)
+	volumes = append(volumes, *getPayloadSecretVolumeMount(job.Name)...)
+	job.Spec.Template.Spec.Volumes = volumes
+	job.Spec.Template.Spec.Containers = containers
+
+	return &job, nil
+}
+
+func getVolumes(namespace string, environment string, component *radixv1.RadixDeployJobComponent) []corev1.Volume {
+	return deployment.GetVolumes(namespace, environment, component.Name, &component.VolumeMounts)
+}
+
+func createJob(appName string, namespace string, jobComponentName string) batchv1.Job {
+	jobName := getJobName(jobComponentName)
+	return batchv1.Job{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: jobName,
 			Labels: map[string]string{
-				kube.RadixJobNameLabel:   jobName,
-				"radix-app-name":         appName, // For backwards compatibility. Remove when cluster is migrated
-				kube.RadixAppLabel:       appName,
-				kube.RadixComponentLabel: componentName,
-				kube.RadixJobTypeLabel:   "job-schedule", //TODO: kube.RadixJobTypeJobSchedule
+				kube.RadixAppLabel:                 appName,
+				kube.RadixComponentLabel:           jobComponentName,
+				kube.RadixJobTypeLabel:             kube.RadixJobTypeJobSchedule,
+				kube.RadixJobNameLabel:             jobName,
+				OBSOLETE_RADIX_APP_NAME_LABEL_NAME: appName, // For backwards compatibility. Remove when cluster is migrated
 			},
 		},
 		Spec: batchv1.JobSpec{
 			Template: corev1.PodTemplateSpec{
 				ObjectMeta: metav1.ObjectMeta{
 					Labels: map[string]string{
-						kube.RadixJobNameLabel: jobName,
+						kube.RadixComponentLabel: jobName,
 					},
-					Namespace: rd.ObjectMeta.Namespace,
+					Namespace: namespace,
 				},
 				Spec: corev1.PodSpec{
-					RestartPolicy: "Never",
-					//InitContainers: initContainers,
-					Containers: getContainers(kubeutil, rd, radixComponent),
-					Volumes: []corev1.Volume{
-						{
-							Name: git.BuildContextVolumeName,
-						},
-						{
-							Name: git.GitSSHKeyVolumeName,
-							VolumeSource: corev1.VolumeSource{
-								Secret: &corev1.SecretVolumeSource{
-									SecretName: git.GitSSHKeyVolumeName,
-									//DefaultMode: &defaultMode,
-								},
-							},
-						},
-						{
-							//Name: azureServicePrincipleSecretName,
-							VolumeSource: corev1.VolumeSource{
-								Secret: &corev1.SecretVolumeSource{
-									//SecretName: azureServicePrincipleSecretName,
-								},
-							},
-						},
-					},
+					RestartPolicy:                corev1.RestartPolicyOnFailure, //TODO: decide what to do with failed job
+					AutomountServiceAccountToken: boolPtr(true),
+					ServiceAccountName:           JOB_SERVICE_ACCOUNT_NAME,
 				},
 			},
 		},
 	}
-	return &job, nil
+}
+
+func getJobName(jobComponentName string) string {
+	timestamp := time.Now().Format("20060102150405")
+	jobTag := strings.ToLower(utils.RandString(8))
+	return fmt.Sprintf("%s-%s-%s", jobComponentName, timestamp, jobTag)
+}
+
+func getPayloadSecretVolumeMount(jobName string) *[]corev1.Volume {
+	volumes := []corev1.Volume{
+		{
+			Name: JOB_PAYLOAD_PROPERTY_NAME,
+			VolumeSource: corev1.VolumeSource{
+				Secret: &corev1.SecretVolumeSource{
+					SecretName: getPayloadSecretName(jobName),
+				},
+			},
+		},
+	}
+	return &volumes
+}
+
+func getPayloadSecretName(jobName string) string {
+	return fmt.Sprintf("%s-%s", JOB_PAYLOAD_PROPERTY_NAME, jobName)
+}
+
+func boolPtr(value bool) *bool {
+	return &value
 }
 
 func getRadixJobComponentBy(rd *radixv1.RadixDeployment, componentName string) *radixv1.RadixDeployJobComponent {
@@ -181,11 +231,12 @@ func getRadixJobComponentBy(rd *radixv1.RadixDeployment, componentName string) *
 }
 
 func getContainers(kube *kube.Kube, rd *radixv1.RadixDeployment, radixJobComponent *radixv1.RadixDeployJobComponent) []corev1.Container {
-	environmentVariables := deployment.GetEnvironmentVariablesFromRadixDeployJobComponent("", kube, rd, radixJobComponent)
+	environmentVariables := deployment.GetEnvironmentVariablesFromRadixDeployJobComponent(rd.Spec.AppName, kube, rd, radixJobComponent)
 	container := corev1.Container{
-		Name:            radixJobComponent.Name,
-		Image:           radixJobComponent.Image,
-		ImagePullPolicy: corev1.PullIfNotPresent,
+		Name:  radixJobComponent.Name,
+		Image: radixJobComponent.Image,
+		//TODO: add setting to be PullAlways, PullIfNotPresent, PullNever. Image should be pulled on job's InitContainer
+		ImagePullPolicy: corev1.PullAlways,
 		Env:             environmentVariables,
 		VolumeMounts:    deployment.GetRadixDeployJobComponentVolumeMounts(radixJobComponent),
 	}
--- models/job_component_description.go
+++ models/job_component_description.go
@@ -3,21 +3,16 @@ package models
 // JobScheduleDescription holds description about scheduling job
 // swagger:model JobScheduleDescription
 type JobScheduleDescription struct {
-	// ComponentName which job is scheduling
-	// required: true
-	// example: componentA
-	ComponentName string `json:"componentName"`
-
-	// Environment which job is scheduling component for
-	// required: true
-	// example: development
-	Environment string `json:"environment"`
-
 	// RadixDeploymentName holding details about scheduling component
 	//
 	// required: true
 	// example: server-abc-dfg
 	RadixDeploymentName string `json:"radixDeploymentName"`
+
+	// ComponentName which job is scheduling
+	// required: true
+	// example: componentAbc
+	ComponentName string `json:"componentName"`
 
 	// Payload holding json data to be mapped to component
 	//
