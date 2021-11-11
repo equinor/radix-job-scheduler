8afc0866816148fe4f6205fa1bd777bd7886fe37
Sergey Smolnikov <ssmol@equinor.com>
2021-11-11 10:56:11 +0100

    136635 add missed status for scheduled job (#42)
    * Extend job status with pod state and event message
    
    * Extracted logic to server repo
    
    * Added created field to status
    
    * Added pod status condition check, when there is no container states
    
    * Removed workflow, which is moved to server
    
    * Renewed radix-operator version

--- README.md
+++ README.md
@@ -2,20 +2,11 @@
 The job scheduler for application jobs
 
 ## Usage
-Request from application container URLs
-* `POST` `http://<job-name>:8080/api/v1/jobs` - start new job 
-* `GET` `http://<job-name>:8080/api/v1/jobs` - get job list
-* `GET` `http://<job-name>:8080/api/v1/jobs/<job-name>` - get job status 
-* `DELETE` `http://<job-name>:8080/api/v1/jobs/<job-name>` - stop and delete job 
+Use [Radix job scheduler server](https://github.com/equinor/radix-job-scheduler-server) or this component as a reference to work with scheduled jobs.
 
 ## Developing
 
 You need Go installed. Make sure `GOPATH` and `GOROOT` are properly set up.
-
-Also needed:
-
-- [`go-swagger`](https://github.com/go-swagger/go-swagger) (on a Mac, you can install it with Homebrew: `brew install go-swagger`)
-- [`statik`](https://github.com/rakyll/statik) (install with `go get github.com/rakyll/statik`)
 
 Clone the repo into your `GOPATH` and run `go mod download`.
 
@@ -28,30 +19,10 @@ Run `make generate-mock` to regenerate mocks
 
 #### Update version
 We follow the [semantic version](https://semver.org/) as recommended by [go](https://blog.golang.org/publishing-go-modules).
-`radix-api` has three places to set version
-* `apiVersionRoute` in `router/server.go` and `BasePath`in `docs/docs.go` - API version, used in API's URL
-* `Version` in `docs/docs.go` - indicates changes in radix-job-scheduler logic - to see (e.g in swagger), that the version in the environment corresponds with what you wanted
-
-  Run following command to update version in `swagger.json`
-    ```
-    make swagger
-    ``` 
-
-* `tag` in git repository (in `main` branch) - matching to the version of `Version` in `docs/docs.go`
+* `tag` in git repository (in `main` branch)
 
   Run following command to set `tag` (with corresponding version)
     ```
     git tag v1.0.0
     git push origin v1.0.0
     ```
-
-### Custom configuration
-
-By default `Info` and `Error` messages are logged. This can be configured via environment variable `LOG_LEVEL` (pods need to be restarted after changes)
-* `LOG_LEVEL=ERROR` - log only `Error` messages
-* `LOG_LEVEL=INFO` or not set - log `Info` and `Error` messages
-* `LOG_LEVEL=WARNING` or not set - log `Info`, `Warning` and `Error` messages
-* `LOG_LEVEL=DEBUG` - log `Debug`, `Warning`, `Info` and `Error` messages
-
-By default `swagger UI` is not available. This can be configured via environment variable `USE_SWAGGER`
-* `USE_SWAGGER=true` - allows to use swagger UI with URL `<api-endpoint>/swaggerui`
--- models/job_status.go
+++ models/job_status.go
@@ -1,8 +1,15 @@
 package models
 
 import (
-	"github.com/equinor/radix-job-scheduler/api/utils"
+	"context"
+	"fmt"
+	"github.com/equinor/radix-common/utils"
 	v1 "k8s.io/api/batch/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"sort"
+	"strings"
 )
 
 // JobStatus holds general information about job status
@@ -12,6 +19,12 @@ type JobStatus struct {
 	// required: true
 	// example: calculator
 	Name string `json:"name"`
+
+	// Created timestamp
+	//
+	// required: true
+	// example: 2006-01-02T15:04:05Z
+	Created string `json:"created"`
 
 	// Started timestamp
 	//
@@ -31,14 +44,100 @@ type JobStatus struct {
 	// Enum: Waiting,Running,Succeeded,Stopping,Stopped,Failed
 	// example: Waiting
 	Status string `json:"status"`
+
+	// Status message, if any, of the job
+	//
+	// required: false
+	// example: "Error occurred"
+	Message string `json:"message"`
 }
 
 // GetJobStatusFromJob Gets job from a k8s job
-func GetJobStatusFromJob(job *v1.Job) *JobStatus {
-	return &JobStatus{
+func GetJobStatusFromJob(kubeClient kubernetes.Interface, job *v1.Job, jobPods []corev1.Pod) *JobStatus {
+	jobStatus := JobStatus{
 		Name:    job.GetName(),
+		Created: utils.FormatTime(&job.ObjectMeta.CreationTimestamp),
 		Started: utils.FormatTime(job.Status.StartTime),
 		Ended:   utils.FormatTime(job.Status.CompletionTime),
-		Status:  GetStatusFromJobStatus(job.Status).String(),
 	}
+	status := GetStatusFromJobStatus(job.Status)
+	jobStatus.Status = status.String()
+	if status != Running {
+		return &jobStatus
+	}
+	for _, pod := range jobPods {
+		if len(pod.Status.ContainerStatuses) > 0 {
+			cs := pod.Status.ContainerStatuses[0]
+			if cs.Ready {
+				continue
+			}
+			switch {
+			case cs.State.Terminated != nil:
+				jobStatus.Status = Stopped.String()
+				jobStatus.Message = cs.State.Terminated.Message
+				return &jobStatus
+			case cs.State.Waiting != nil:
+				jobStatus.Status = Waiting.String()
+				jobStatus.Started = ""
+				message := cs.State.Waiting.Message
+				if len(message) > 0 {
+					jobStatus.Message = message
+					return &jobStatus
+				}
+				jobStatus.Message = getLastEventMessageForPod(kubeClient, pod)
+				if len(jobStatus.Message) == 0 {
+					jobStatus.Message = "Job has not been started. If it takes long time to start, please check an events list for a reason."
+				}
+				return &jobStatus
+			}
+			continue
+		}
+		if len(pod.Status.Conditions) > 0 {
+
+			lastCondition := sortPodStatusConditionsDesc(pod.Status.Conditions)[0]
+			if lastCondition.Status == corev1.ConditionTrue {
+				continue
+			}
+			jobStatus.Status = Waiting.String()
+			jobStatus.Message = fmt.Sprintf("%s %s", lastCondition.Reason, lastCondition.Message)
+		}
+	}
+	return &jobStatus
+}
+
+func getLastEventMessageForPod(kubeClient kubernetes.Interface, pod corev1.Pod) string {
+	eventsList, err := kubeClient.CoreV1().Events(pod.Namespace).List(context.Background(), metav1.ListOptions{})
+	if err != nil {
+		return ""
+	}
+	events := sortEventsDesc(eventsList.Items)
+	for _, event := range events {
+		if event.InvolvedObject.Name == pod.Name {
+			if strings.Contains(event.Message, "container init was OOM-killed (memory limit too low?)") {
+				return fmt.Sprintf("Probably memory limit too low. Error: %s", event.Message)
+			}
+			return event.Message
+		}
+	}
+	return ""
+}
+
+func sortEventsDesc(events []corev1.Event) []corev1.Event {
+	sort.Slice(events, func(i, j int) bool {
+		if events[i].CreationTimestamp.IsZero() || events[j].CreationTimestamp.IsZero() {
+			return false
+		}
+		return events[j].CreationTimestamp.Before(&events[i].CreationTimestamp)
+	})
+	return events
+}
+
+func sortPodStatusConditionsDesc(podConditions []corev1.PodCondition) []corev1.PodCondition {
+	sort.Slice(podConditions, func(i, j int) bool {
+		if podConditions[i].LastTransitionTime.IsZero() || podConditions[j].LastTransitionTime.IsZero() {
+			return false
+		}
+		return podConditions[j].LastTransitionTime.Before(&podConditions[i].LastTransitionTime)
+	})
+	return podConditions
 }
