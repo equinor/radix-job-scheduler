122518dcbdc7d8bdef56f25c37dce4d0aa18d60f
Sergey Smolnikov <ssmol@equinor.com>
2024-07-05 14:46:30 +0200

    Adding unit-tests

--- pkg/batch/batch_test.go
+++ pkg/batch/batch_test.go
@@ -5,19 +5,44 @@ import (
 	"reflect"
 	"testing"
 
+	"github.com/equinor/radix-common/utils/numbers"
+	testUtil "github.com/equinor/radix-job-scheduler/internal/test"
 	modelsv2 "github.com/equinor/radix-job-scheduler/models/v2"
+	"github.com/equinor/radix-operator/pkg/apis/kube"
 	radixv1 "github.com/equinor/radix-operator/pkg/apis/radix/v1"
+	"github.com/equinor/radix-operator/pkg/apis/utils"
+	operatorUtils "github.com/equinor/radix-operator/pkg/apis/utils"
+	radixLabels "github.com/equinor/radix-operator/pkg/apis/utils/labels"
 	"github.com/equinor/radix-operator/pkg/client/clientset/versioned"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+type testProps struct {
+	appName               string
+	envName               string
+	radixJobComponentName string
+	radixDeploymentName   string
+}
+
 func TestCopyRadixBatchOrJob(t *testing.T) {
+	props := testProps{
+		appName:               "any-app",
+		envName:               "any-env",
+		radixJobComponentName: "any-job",
+		radixDeploymentName:   "any-deployment",
+	}
+	const (
+		batchName1 = "batch1"
+		jobName1   = "job1"
+		jobName2   = "job2"
+	)
 	type args struct {
-		ctx                     context.Context
-		radixClient             versioned.Interface
-		sourceRadixBatch        *radixv1.RadixBatch
-		sourceJobName           string
-		radixDeployJobComponent *radixv1.RadixDeployJobComponent
-		radixDeploymentName     string
+		sourceRadixBatch              *radixv1.RadixBatch
+		sourceJobName                 string
+		batchRadixDeployJobComponent  *operatorUtils.DeployJobComponentBuilder
+		activeRadixDeployJobComponent *operatorUtils.DeployJobComponentBuilder
 	}
 	tests := []struct {
 		name    string
@@ -25,20 +50,73 @@ func TestCopyRadixBatchOrJob(t *testing.T) {
 		want    *modelsv2.RadixBatch
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				sourceRadixBatch:             createRadixBatch(props, batchName1, kube.RadixBatchTypeBatch, []string{jobName1, jobName2}, nil),
+				sourceJobName:                "",
+				batchRadixDeployJobComponent: createRadixDeployJobComponent(props),
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := CopyRadixBatchOrJob(tt.args.ctx, tt.args.radixClient, tt.args.sourceRadixBatch, tt.args.sourceJobName, tt.args.radixDeployJobComponent, tt.args.radixDeploymentName)
+			radixClient, _, _, _ := testUtil.SetupTest(props.appName, props.envName, props.radixJobComponentName, props.radixDeploymentName, 1)
+			radixClient.RadixV1().RadixDeployments(utils.GetEnvironmentNamespace(props.appName, props.envName)).
+				Create(context.Background(), utils.ARadixDeployment().WithComponent().BuildRD(), metav1.CreateOptions{})
+			require.NoError(t)
+			createdRadixBatchStatus, err := CopyRadixBatchOrJob(context.Background(), radixClient, tt.args.sourceRadixBatch, tt.args.sourceJobName, tt.args.batchRadixDeployJobComponent, props.radixDeploymentName)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("CopyRadixBatchOrJob() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("CopyRadixBatchOrJob() got = %v, want %v", got, tt.want)
-			}
+			assert.NotNil(t, createdRadixBatchStatus, "Status is nil")
+			// if !reflect.DeepEqual(got, tt.want) {
+			// 	t.Errorf("CopyRadixBatchOrJob() got = %v, want %v", got, tt.want)
+			// }
 		})
 	}
+}
+
+func createRadixDeployJobComponent(props testProps, rules ...radixv1.BatchStatusRule) *operatorUtils.DeployJobComponentBuilder {
+	return &aRadixDeploymentWithComponentModifier(props, func(builder operatorUtils.DeployJobComponentBuilder) operatorUtils.DeployJobComponentBuilder {
+		return builder.WithBatchStatusRules(rules...)
+	})
+}
+
+func createRadixBatch(props testProps, batchName string, radixBatchType kube.RadixBatchType, jobNames []string, jobStatuses map[string]radixv1.RadixBatchJobPhase) *radixv1.RadixBatch {
+	radixBatch := radixv1.RadixBatch{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: batchName,
+			Labels: radixLabels.Merge(
+				radixLabels.ForApplicationName(props.appName),
+				radixLabels.ForComponentName(props.radixJobComponentName),
+				radixLabels.ForBatchType(radixBatchType),
+			),
+		},
+		Spec: radixv1.RadixBatchSpec{
+			RadixDeploymentJobRef: radixv1.RadixDeploymentJobComponentSelector{
+				LocalObjectReference: radixv1.LocalObjectReference{Name: props.radixDeploymentName},
+				Job:                  props.radixJobComponentName,
+			},
+		},
+	}
+	for _, jobName := range jobNames {
+		radixBatch.Spec.Jobs = append(radixBatch.Spec.Jobs, radixv1.RadixBatchJob{
+			Name: jobName,
+		})
+	}
+	if jobStatuses != nil {
+		for _, jobName := range jobNames {
+			if jobPhase, ok := jobStatuses[jobName]; ok {
+				radixBatch.Status.JobStatuses = append(radixBatch.Status.JobStatuses, radixv1.RadixBatchJobStatus{
+					Name:  jobName,
+					Phase: jobPhase,
+				})
+			}
+		}
+	}
+	return &radixBatch
 }
 
 func TestDeleteRadixBatch(t *testing.T) {
@@ -318,4 +396,20 @@ func Test_updateRadixBatch(t *testing.T) {
 			}
 		})
 	}
+}
+
+func aRadixDeploymentWithComponentModifier(props testProps, m func(builder operatorUtils.DeployJobComponentBuilder) operatorUtils.DeployJobComponentBuilder) operatorUtils.DeploymentBuilder {
+	builder := operatorUtils.NewDeploymentBuilder().
+		WithAppName(props.appName).
+		WithImageTag("imagetag").
+		WithEnvironment(props.envName).
+		WithJobComponent(m(operatorUtils.NewDeployJobComponentBuilder().
+			WithName(props.radixJobComponentName).
+			WithImage("radixdev.azurecr.io/job:imagetag").
+			WithSchedulerPort(numbers.Int32Ptr(8080))))
+	return builder
+}
+
+func aRadixDeployment(props testProps) operatorUtils.DeploymentBuilder {
+	return aRadixDeploymentWithComponentModifier(props, func(builder operatorUtils.DeployJobComponentBuilder) operatorUtils.DeployJobComponentBuilder { return builder })
 }
